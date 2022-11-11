623da56102ddefbacce26c13551f59a80a3a3232
alexta <terpyakov@gmail.com>
2022-11-11 21:59:02 +0300

    review

--- src/utils.ts
+++ src/utils.ts
@@ -1,9 +1,8 @@
-import { URL } from 'node:url'
 import crypto from 'node:crypto'
 import fs from 'node:fs'
 import path from 'node:path'
 
-const publicCert = fs.readFileSync(path.join(__dirname, '../certs/gm.crt')).toString()
+const publicCert = fs.readFileSync(path.join(__dirname, './certs/gm.crt')).toString()
 
 export type RsaKey = Parameters<crypto.Sign['sign']>[0]
 export type HmacKey = Parameters<typeof crypto.createHmac>[1]
